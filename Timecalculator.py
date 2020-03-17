8788732c7ad64610d6ad359eace341849045a138
phoomct <57429408+phoomct@users.noreply.github.com>
2020-03-17 23:01:09 +0700

    Create Timecalculator.py

---
+++ Timecalculator.py
@@ -0,0 +1,7 @@
+""" Time Calculator """
+def main():
+    """ Write a program that take 3 numbers from user including number of hours,
+    number of minutes and number of seconds and return the
+    total number of seconds to the user. """
+    print((int(input())*3600) + (int(input()) * 60) + (int(input())))
+main()
