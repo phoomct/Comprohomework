66d0667a29b0d4c794db1d6aa46c816bb4f8c018
phoomct <57429408+phoomct@users.noreply.github.com>
2020-03-17 21:38:57 +0700

    Create averageNumber.py

---
+++ averageNumber.py
@@ -0,0 +1,13 @@
+""" averageNumber """
+def main():
+    """ Write a program that compute the
+    average from 5 numbers given by the user """
+    num = float(input())
+    num2 = float(input())
+    num3 = float(input())
+    num4 = float(input())
+    num5 = float(input())
+    ans = (num + num2 + num3 + num4 + num5) / 5
+    print(ans)
+main()
+    
