ee2d712af34bdfae7e4ea77ed449db826b99c550
phoomct <57429408+phoomct@users.noreply.github.com>
2020-03-17 21:39:06 +0700

    Update initailString.py

--- initailString.py
+++ initailString.py
@@ -1,6 +1,7 @@
 """ initail String """
 def main():
-    """ Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string.  """
+    """ Write a Python program to get a string made of the
+    first 2 and the last 2 chars from a given a string.  """
     data = input()
     st1 = data[0:2]
     data = data[::-1]
