84d5e57bbb9ba5744eb32746c02e6d4870dffb34
phoomct <57429408+phoomct@users.noreply.github.com>
2020-03-17 22:01:22 +0700

    Create combineString.py

---
+++ combineString.py
@@ -0,0 +1,8 @@
+""" combineString """
+def main():
+    """ Write a Python program to get a single string from two given strings, s
+    eparated by a space and swap the first two characters of each string.  """
+    data = input()
+    data2 = input()
+    print(data2 +" " + data)
+main()
