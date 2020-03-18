4fa5286b0b15086bca1e4fdff90aca870c95c54b
phoomct <57429408+phoomct@users.noreply.github.com>
2020-03-18 08:50:52 +0700

    Create Lighting distance.py

---
+++ Lighting distance.py
@@ -0,0 +1,7 @@
+""" Lighting distance """
+def main():
+    """rite a program that detemines the distance to a lighting strike based on th
+     e time elapsed between the flash and the sound of thunder.
+     The speed of sound is approximately 1100 ft/sec and 1 mile is 5280 ft."""
+    print((1100 * float(input())) / 5280)
+main()
